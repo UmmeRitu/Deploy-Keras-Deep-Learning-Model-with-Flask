ca2b6c1b004d6ab2b4c67e3d549dfe1dd6613711
shayekh00 <shayekhnavid@gmail.com>
2023-06-03 16:02:42 +0200

    commit

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ transformer = joblib.load("assets/data_transformer2.joblib")
 @app.route("/", methods=["POST"])
 def index():
     print("This came here")
+    print(request)
     data = request.json
     df = pd.DataFrame(data, index=[0])
     prediction = model.predict(transformer.transform(df))
     predicted_price = expm1(prediction.flatten()[0])
-    return jsonify({  "message": "Hello, world!"})
+    # return jsonify({  "message": "Hello, world!"})
+    return jsonify({"price": str(predicted_price)})
