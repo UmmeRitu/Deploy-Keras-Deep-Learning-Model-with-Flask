83b7e2df9fe895b7829791ee86d4a86f46ec9493
shayekh00 <shayekhnavid@gmail.com>
2023-06-03 17:18:01 +0200

    iris

--- app.py
+++ app.py
@@ -3,20 +3,22 @@ from math import expm1
 import joblib
 import pandas as pd
 from flask import Flask, jsonify, request
-from tensorflow import keras
+# from tensorflow import keras
+import pickle
 
 app = Flask(__name__)
-model = keras.models.load_model("assets/price_prediction_model3.h5")
-transformer = joblib.load("assets/data_transformer3.joblib")
-
+# model = keras.models.load_model("assets/price_prediction_model3.h5")
+# transformer = joblib.load("assets/data_transformer3.joblib")
+model = pickle.load(open(filename, 'rb'))
 
 @app.route("/", methods=["POST"])
 def index():
     print("This came here")
     print(request)
     data = request.json
-    df = pd.DataFrame(data, index=[0])
-    prediction = model.predict(transformer.transform(df))
-    predicted_price = expm1(prediction.flatten()[0])
-    # return jsonify({  "message": "Hello, world!"})
-    return jsonify({"price": str(predicted_price)})
+    prediction = model.predict([[6.8, 3.2, 5.9, 2.3]])
+    # df = pd.DataFrame(data, index=[0])
+    # prediction = model.predict(transformer.transform(df))
+    # predicted_price = expm1(prediction.flatten()[0])
+    # # return jsonify({  "message": "Hello, world!"})
+    return jsonify({"Specis": str(prediction[0])})
