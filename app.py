28c7d9682f3581ae94d0d535b075cd9acfd4dc1f
shayekh00 <shayekhnavid@gmail.com>
2023-06-03 14:27:05 +0200

    model update

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from flask import Flask, jsonify, request
 from tensorflow import keras
 
 app = Flask(__name__)
-model = keras.models.load_model("assets/price_prediction_model.h5")
-transformer = joblib.load("assets/data_transformer.joblib")
+model = keras.models.load_model("assets/price_prediction_model2.h5")
+transformer = joblib.load("assets/data_transformer2.joblib")
 
 
 @app.route("/", methods=["POST"])
@@ -15,6 +15,6 @@ def index():
     print("This came here")
     data = request.json
     df = pd.DataFrame(data, index=[0])
-    # prediction = model.predict(transformer.transform(df))
-    # predicted_price = expm1(prediction.flatten()[0])
+    prediction = model.predict(transformer.transform(df))
+    predicted_price = expm1(prediction.flatten()[0])
     return jsonify({  "message": "Hello, world!"})
